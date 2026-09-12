c66574a415fd0779832d957da9744837a34745fd
rbjack2017 <105751213+rbjack2017@users.noreply.github.com>
2026-09-12 19:41:40 -0400

    Update sidebar.js

--- docs/assets/js/sidebar.js
+++ docs/assets/js/sidebar.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Load sidebar HTML
     fetch("includes/sidebar.html")
         .then(res => res.text())
         .then(html => {
             document.getElementById("sidebar").innerHTML = html;
+
+            // Sidebar toggle
+            const toggle = document.getElementById("sidebarToggle");
+            const sidebar = document.querySelector(".sidebar");
+
+            toggle.addEventListener("click", () => {
+                sidebar.classList.toggle("collapsed");
+            });
         });
 });
